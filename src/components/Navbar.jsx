9c3ce8d22ed13255873bf68c1581ddf08c6d4796
Dylan-f1 <dylan.fernandes.travail@gmail.com>
2025-06-01 13:31:47 +0200

    feat(responsive) rajout responsivité

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <div className="navbar-container">
-      
       <div className="navigation-container">
         <div className="logo">
-          <Link to="/">DF</Link>
+          <Link to="/" onClick={closeMenu}>DF</Link>
         </div>
+        
+        {/* Menu hamburger */}
+        <div 
+          className={`hamburger ${isMenuOpen ? 'active' : ''}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Navigation desktop */}
         <nav className="navigation">
           <Link to="/competences" className="nav-link">
             COMPÉTENCES
@@ -18,16 +39,38 @@ const Navbar = () => {
             CV
           </Link>
           <Link to="/projets" className="nav-link">
-            PROJETS PERSONNELS
+            PROJETS
           </Link>
           <Link to="/entreprise" className="nav-link">
             ENTREPRISE
           </Link>
           <Link to="/vielle-techno" className="nav-link">
-            VIEILLE TECHNOLOGIE
+            TECHNOLOGIE
           </Link>
           <Link to="/a-propos" className="nav-link">
-            A PROPOS
+            À PROPOS
+          </Link>
+        </nav>
+
+        {/* Navigation mobile */}
+        <nav className={`navigation mobile ${isMenuOpen ? 'active' : ''}`}>
+          <Link to="/competences" className="nav-link" onClick={closeMenu}>
+            COMPÉTENCES
+          </Link>
+          <Link to="/cv" className="nav-link" onClick={closeMenu}>
+            CV
+          </Link>
+          <Link to="/projets" className="nav-link" onClick={closeMenu}>
+            PROJETS
+          </Link>
+          <Link to="/entreprise" className="nav-link" onClick={closeMenu}>
+            ENTREPRISE
+          </Link>
+          <Link to="/vielle-techno" className="nav-link" onClick={closeMenu}>
+            TECHNOLOGIE
+          </Link>
+          <Link to="/a-propos" className="nav-link" onClick={closeMenu}>
+            À PROPOS
           </Link>
         </nav>
       </div>
