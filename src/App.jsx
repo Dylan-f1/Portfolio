7a7fd289e6fb78d64fb03a5441ebb413f8d5216d
Dylan-f1 <dylan.fernandes.travail@gmail.com>
2025-06-09 19:57:31 +0200

    feat(debug) debug vercel 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Competences from './Pages/Competences';
-import CV from './Pages/CV';
-import Projets from './Pages/Projets';
-import APropos from './Pages/A-Propos';
-import VeilleTechno from './Pages/Veille-Techno';
-import Entreprise from './Pages/Entreprise';
+import Competences from './Pages/Competences.jsx';
+import CV from './Pages/CV.jsx';
+import Projets from './Pages/Projets.jsx';
+import APropos from './Pages/A-Propos.jsx';
+import VeilleTechno from './Pages/Veille-Techno.jsx';
+import Entreprise from './Pages/Entreprise.jsx';
 import './App.css';
 
 function App() {
